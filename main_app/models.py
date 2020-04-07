4a4f74f54c8ae31f9c983316ed430166e2ee4494
Tina <longevity.tina@gmail.com>
2020-04-07 14:41:34 -0700

    db updates

--- main_app/models.py
+++ main_app/models.py
@@ -4,7 +4,10 @@ from django.db import models
 
 
 class Scent(models.Model):
-    name = models.CharField(max_length=100),
-    location = models.CharField(max_length=100),
-    characteristics = models.TextField(max_length=100),
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    characteristics = models.TextField(max_length=100)
     rating = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
-
+from .models import Scent
 from django.http import HttpResponse
 
-# Define the home view
 
+# views-----------
 
 def home(request):
     return HttpResponse('<h1>Scent Collector (✿ヘᴥヘ)</h1>')
@@ -16,19 +14,5 @@ def about(request):
 
 
 def scents_index(request):
+    scents = Scent.objects.all()
     return render(request, 'scents/index.html', {'scents': scents})
-
-
-class Scent:  # Note that parens are optional if not inheriting from another class
-    def __init__(self, name, location, characteristics, rating):
-        self.name = name
-        self.location = location
-        self.characteristics = characteristics
-        self.rating = rating
-
-
-scents = [
-    Scent('Tree', 'In front of home', 'Woodsy, bark, touch of pee', 6),
-    Scent('Pole', '23rd & Florida', 'Metalic, urine, frequently visited', 2),
-    Scent('Marc', 'Valencia & 19th', 'Sweet, savory, with notes of mountain', 10)
-]
