11c277afeb46984e90887124ed436f4c2f572146
Tina <longevity.tina@gmail.com>
2020-04-07 21:42:35 -0700

    Registers wafting class to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Scent
+from .models import Scent, Wafting
 
 admin.site.register(Scent)
+admin.site.register(Wafting)
