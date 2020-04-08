322f3121d5b6e7e9d54382d247bc3a212438c968
Tina <longevity.tina@gmail.com>
2020-04-07 18:48:14 -0700

    Adds Wafting class and field.choices

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 # Create your models here.
+TIMES = (
+    ('M', 'Morning'),
+    ('A', 'Afternoon'),
+    ('E', 'Evening')
+)
+
+EMOTIONS = (
+    ('H', 'Happy'),
+    ('S', 'Sad'),
+    ('H', 'Humorous'),
+    ('A', 'Angry')
+)
 
 
 class Scent(models.Model):
@@ -11,3 +23,21 @@ class Scent(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Wafting(models.Model):
+    date = models.DateField()
+    time = models.CharField(
+        max_length=1,
+        choices=TIMES,
+        default=TIMES[0][0]
+    )
+    emotion = models.CharField(
+        max_length=1,
+        choices=EMOTIONS,
+        default=EMOTIONS[0][0],
+    )
+    scent = models.ForeignKey(Scent, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"This smell made you feel {self.get_emotion_display()} in the {self.get_time_display} on {self.date}"
