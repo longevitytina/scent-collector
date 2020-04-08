672fa87a0bfabef88e236ac5c819ed6475495ed5
Tina <longevity.tina@gmail.com>
2020-04-07 22:19:46 -0700

    Adds add_emotion function

--- main_app/views.py
+++ main_app/views.py
@@ -23,3 +23,15 @@ def scents_detail(request, scent_id):
     scent = Scent.objects.get(id=scent_id)
     wafting_form = WaftingForm()
     return render(request, 'scents/detail.html', {'scent': scent, 'wafting_form': wafting_form})
+
+
+def add_emotion(request, scent_id):
+    form = WaftingForm(request.POST)
+  # validate the form
+    if form.is_valid():
+        # don't save the form to the db until it
+        # has the scent_id assigned
+        new_emotion = form.save(commit=False)
+        new_emotion.scent_id = scent_id
+        new_emotion.save()
+    return redirect('detail', scent_id=scent_id)
