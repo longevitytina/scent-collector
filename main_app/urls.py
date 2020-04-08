b8248d67e4ed7dd98fa6bb1dda82ef04cd94a316
Tina <longevity.tina@gmail.com>
2020-04-07 22:19:16 -0700

    Adds form paths

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('scents/', views.scents_index, name='index'),
-    path('scents/<int:scent_id>/', views.scents_detail, name='detail')
+    path('scents/<int:scent_id>/', views.scents_detail, name='detail'),
+    path('scents/<int:scent_id>/add_emotion',
+         views.add_emotion, name='add_emotion')
 ]
