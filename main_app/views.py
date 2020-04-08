30edc4c7ea8fed067d2f11f542267b7394666a17
Tina <longevity.tina@gmail.com>
2020-04-08 14:53:09 -0700

    Adds imports and Power classes

--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, redirect
-from .models import Scent
-from django.http import HttpResponse
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+
+from .models import Scent, Power
 from .forms import WaftingForm
 
 
 # views-----------
 
 def home(request):
-    return HttpResponse('<h1>Scent Collector (✿ヘᴥヘ)</h1>')
+    return render(request, 'home.html')
 
 
 def about(request):
@@ -21,6 +24,8 @@ def scents_index(request):
 
 def scents_detail(request, scent_id):
     scent = Scent.objects.get(id=scent_id)
+    # powers_scent_doesnt_have = Power.object.exclude(
+    #     id_in=scent.powers.all().values_list('id'))
     wafting_form = WaftingForm()
     return render(request, 'scents/detail.html', {'scent': scent, 'wafting_form': wafting_form})
 
@@ -35,3 +40,21 @@ def add_emotion(request, scent_id):
         new_emotion.scent_id = scent_id
         new_emotion.save()
     return redirect('detail', scent_id=scent_id)
+
+
+class PowerList(ListView):
+    model = Power
+
+
+class PowerDetail(DetailView):
+    model = Power
+
+
+class PowerUpdate(UpdateView):
+    model = Power
+    fields = ['name', 'description']
+
+
+class PowerDelete(DeleteView):
+    model = Power
+    success_url = '/powers/'
