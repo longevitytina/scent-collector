675906d7ca770f27ed9b2f03f1ba931cfdc84226
Tina <longevity.tina@gmail.com>
2020-04-08 15:56:46 -0700

    Registers power

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Scent, Wafting
+from .models import Scent, Wafting, Power
 
 admin.site.register(Scent)
 admin.site.register(Wafting)
+admin.site.register(Power)
